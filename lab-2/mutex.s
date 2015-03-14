d1ec87ca7b217ea6330a9dfb300413352922c9f8
peter1yu <peter1yu@yahoo.com.tw>
2015-03-14 21:20:59 +0800

    add Lab-2

--- lab-2/mutex.s
+++ lab-2/mutex.s
@@ -9,6 +9,11 @@
 	.type lock_mutex, function
 lock_mutex:
         @ INSERT CODE BELOW
+	ldr r1, =locked
+.L1:
+	ldrex r2, [r0]
+	cmp r2, #0
+	bne .L1
 
         @ END CODE INSERT
 	bx lr
@@ -19,6 +24,8 @@ lock_mutex:
 	.type unlock_mutex, function
 unlock_mutex:
 	@ INSERT CODE BELOW
+	ldr r1, =unlocked
+	str r1, [r0]
         
         @ END CODE INSERT
 	bx lr
